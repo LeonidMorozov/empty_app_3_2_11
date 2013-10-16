a3496aad0ee5693fc6d38d21b7f3a128783f9814
LeonidMorozov <lmmoby98@yandex.ru>
2013-10-16 17:57:01 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmptyApp3211::Application.config.secret_token = '3426e74e7b2309caec6d2df4367932d444f0b72d3ae9212a31b6bd13f816d1bfbf198d6ebc0772dc6918b360b1afa6394cfcbf1a6e96ced8ef9be85d8bff68f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EmptyApp3211::Application.config.session_store :cookie_store, key: '_empty_app_3_2_11_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EmptyApp3211::Application.config.session_store :active_record_store
