6220175572a847d4cfba52a51eedd9cfd4cfbc44
LeonidMorozov <lmmoby98@yandex.ru>
2013-10-22 16:21:05 -0700

    + play w/ puma config

--- config/puma.rb
+++ config/puma.rb
@@ -13,13 +13,13 @@ preload_app!
 #	end
 #end
 
-#on_worker_boot do
-#	ActiveRecord::Base.connection_pool.disconnect!
-#
-#	ActiveSupport.on_load(:active_record) do
-#		config = Rails.application.config.database_configuration[Rails.env]
-#		config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-#		config['pool']              = ENV['DB_POOL'] || 5
-#		ActiveRecord::Base.establish_connection
-#	end
-#end
+on_worker_boot do
+	ActiveRecord::Base.connection_pool.disconnect!
+
+	ActiveSupport.on_load(:active_record) do
+		config = Rails.application.config.database_configuration[Rails.env]
+		config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+		config['pool']              = ENV['DB_POOL'] || 5
+		ActiveRecord::Base.establish_connection
+	end
+end
